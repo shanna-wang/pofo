6882139f535b6e36a071d26024d6e1f659a7f6c0
Shanna Wang <lashimmer@gmail.com>
2016-10-06 17:42:36 -0700

    add projects

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Shanna Wang' });
 });
+
+router.get('/challenge', function(req, res, next) {
+  res.render('challenge', { title: 'UI Challenge' });
+});
+
+
 
 module.exports = router;
